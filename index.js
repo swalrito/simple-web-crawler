fe01e5eca9864c58e6c6b33c001251f8c6dac892
彭定林 <1979945716@qq.com>
2018-01-30 18:49:54 +0800

    2018.1.30 将express升级至koa。

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-const express = require('express')
+
+const Koa = require('koa')
+const Router=require('koa-router')
 const cheerio = require ('cheerio')
 const superagent = require('superagent')
 
-//创建一个express实例
-const app = express()
+//创建一个koa实例
+const app = new Koa()
+//koa-router实例
+const router=new Router()
 
-//get method path='/'
-app.get('/',function(req,res,next){
-    //储存爬去内容的array
-    let items=[]
-    //superagent moudle use get 访问 'https://cnodejs.org/'
-    superagent
-        .get('https://cnodejs.org/')
-        //superagent get method访问链接，得到返回后的callback
-        .end(function(err,response){
-            if(err){
-                return next(err)
-            }
-            //模仿jquery使用`$`符号作为cheerio操作dom
-            let $ = cheerio.load(response.text)
-            $('#topic_list .topic_title').each(function(index,ele){
-                //获取.topic_title元素的dom
-                let $element=$(ele)
-                items.push({
-                    title:$element.attr('title'),
-                    href:$element.attr('href'),
-                })
+router.get('/crawler',async (ctx,next)=>{
+        //储存爬去内容的array
+        let items=[]
+        //superagent moudle use get 访问 'https://cnodejs.org/'
+        let response=await superagent.get('https://cnodejs.org/')
+        //superagent get method访问链接，得到response
+        //模仿jquery使用`$`符号作为cheerio操作dom
+        let $ = cheerio.load(response.text)
+        $('#topic_list .topic_title').each(function(index,ele){
+            //获取.topic_title元素的dom
+            let $element=$(ele)
+            items.push({
+                title:$element.attr('title'),
+                href:$element.attr('href'),
             })
-            res.send(items)
         })
+        //将数据内容赋值
+        ctx.body=items
         
 })
 
-//启动30000端口
-app.listen(3000,function(){
-    console.log('server is running at port 3000……')
+app.use(async (ctx,next)=>{
+    console.log(`request url: ${ctx.url}`)
+    console.log(`request method: ${ctx.method}`)
+    await next()
+})
+
+// 启动 router.routes()
+app.use(router.routes())
+
+//启动3000端口
+app.listen(3000,()=>{
+    console.log(`app is running at port 3000 ......`)
 })
