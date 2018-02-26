ff0e8ba08f0befa846e0b7ca40c723f4b2f0a437
彭定林 <1979945716@qq.com>
2018-02-26 23:14:47 +0800

    2018.2.26 进一步完善爬虫，爬取cnode社区所有帖子。

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 const Router=require('koa-router')
 const superagent = require('superagent')
 const cheerio = require('cheerio')
-const eventproxy = require('eventproxy')
+const async =require('async')
 const Url = require('url')
-//创建一个eventproxy实例
-const ep =new eventproxy()
 //创建一个router实例
 const router = new Router()
 
 //ajax request url and cheeio 解析首页帖子链接
-const requestUrl=async function(url,arr){
+const requestUrl=async function(url,arr,callback){
+    console.log(`正在获取论坛每一页的帖子，正在访问的url是:${url}`)
     //superagent moudle use get 访问 'https://cnodejs.org/'
     let response=await superagent.get(url)
     //superagent get method访问链接，得到response
@@ -20,40 +19,81 @@ const requestUrl=async function(url,arr){
         let $element=$(ele)
         //使用url拼接完整的url
         let href=Url.resolve(url,$element.attr('href'))
-        arr.push(href)
+        let obj ={}
+        obj.url=href
+        obj.title=$element.attr('title')
+        arr.push(obj)
     })
+    callback(null,url)
 }
 
-//并发一定次数后，将帖子内容的沙发push至数组中
-ep.after('topic_html',40,(topics)=>{
-    topics=topics.map(topic=>{
-        let topicUrl=topic[0] //帖子的url
-        let topicHtml=topic[1] //帖子的内容
-        let $ = cheerio.load(topicHtml)
-        //返回每个obj
-        return ({
-            title: $('.topic_full_title').text().trim(),
-            href: topicUrl,
-            comment1: $('.reply_content').eq(0).text().trim(),
-          })
-    })
-    console.log('final:')
-    console.log(topics)
+let emitUrl=function(topic){
+    let url = topic.url
+    superagent.get(url)
+        .end((err,res)=>{
+            if(err){
+                console.log(err)
+                return 
+            }
+            let html=res.text
+            let $ =cheerio.load(html)
+            topic.comment1=$('.reply_content').eq(0).text().trim()
+        })
+}
 
-})
+//计数器
+let computeNum=0
+
+let fetchUrl = function (topic,callback){
+    let delay =parseInt((Math.random()*10000000%2000),10)
+    //计数器
+    computeNum++
+    console.log(`现在的并发数：${computeNum}，请求url为：${topic.url},耗时：${delay}毫秒`)
+    emitUrl(topic)
+    //delay一定的随机时间进行http请求的callback
+    setTimeout(() => {
+        computeNum--
+        callback(null,topic.url)
+    }, delay)
+}
+
+let getMaxPage=async function(url){
+        let response = await superagent.get(url)
+        let html = response.text
+        let $ = cheerio.load(html)
+        let num =$('.pagination').children().children().last().children().attr('href')
+        return num
+}
 
 router.get('/crawler',async (ctx,next)=>{
     //储存爬取的帖子链接
-    let topicUrls=[]
+    let topics=[]
+    //爬取网站的url
+    let mainUrl=[]
+    //获取社区总页数
+    let num =await getMaxPage('https://cnodejs.org/')
+    let len=num.indexOf('page=')
+    num=num.slice(len+5)
+    for(let i = 1;i<=num;i++){
+        mainUrl.push(`https://cnodejs.org/?tab=all&page=${i}`)
+    }
     //爬取帖子链接并解析内容赋值到数组上
-    await requestUrl('https://cnodejs.org/',topicUrls)
-    //并发请求帖子链接，并且获取每个帖子的沙发内容
-    topicUrls.forEach(function (topicUrl){
-        superagent.get(topicUrl)
-            .end(function (err, res) {
-            console.log('fetch ' + topicUrl + ' successful')
-            ep.emit('topic_html', [topicUrl, res.text])
-            })
+    // for(let url of mainUrl){
+    //     await requestUrl(url,topics)
+    // }
+    async.mapLimit(mainUrl,5,function(url,callback){
+        requestUrl(url,topics,callback)
+    },function(err,result){
+        console.log('获取每页帖子链接完毕')
+        console.log(result)
+        //并发请求帖子链接，并且获取每个帖子的沙发内容
+        async.mapLimit(topics,5,function(topic,callback){
+            fetchUrl(topic,callback)
+        },function(err,result){
+            console.log('final:')
+            console.log(result)
+            console.log(topics)
+        })
     })
     
 })
